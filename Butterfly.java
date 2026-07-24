e6aff8d07369d212125f050c5de8909fc357ea4d
Tarun-kumar <tarunkumarbhyri13@gmail.com>
2026-07-24 14:58:38 +0530

    Add files via upload

---
+++ Butterfly.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class Butterfly
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter a number:");
+	    int n=sc.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=i;j++)
+	        System.out.print("*");
+	        for(int j=1;j<=2*(n-i);j++)
+	        System.out.print(" ");
+	        for(int j=1;j<=i;j++)
+	        System.out.print("*");
+	        System.out.println();
+	        
+	    }
+	    for(int i=n-1;i>=1;i--) {
+	        for(int j=1;j<=i;j++)
+	        System.out.print("*");
+	        for(int j=1;j<=2*(n-i);j++)
+	        System.out.print(" ");
+	        for(int j=1;j<=i;j++)
+	        System.out.print("*");
+	        System.out.println();
+	    }
+	}
+}
+		
