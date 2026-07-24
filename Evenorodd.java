6e91dc845fca9b52d4a8ff603928a60d0bcbf3d3
Tarun-kumar <tarunkumarbhyri13@gmail.com>
2026-07-24 14:55:03 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,15 @@
+
+
+public class Evenorodd
+{
+	public static void main(String[] args) {
+		int a=32;
+		if(a%2==0)
+		{
+		    System.out.println(a +" is even");
+		}
+		else {
+		    System.out.println(a +" is odd");
+		}
+	}
+}
